ded8249caf281228d41738d50ae5da026e622b30
DThanhTung <112098570+teoduongthanh@users.noreply.github.com>
2026-01-07 01:02:56 +0700

    changed local host mysql

--- services/auth-service/prisma.config.ts
+++ services/auth-service/prisma.config.ts
@@ -4,6 +4,6 @@ import 'dotenv/config'
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DATABASE_URL') || "http://127:0.0.1:3306/",
+    url: env('DATABASE_URL') || "your-post-mysql",
   },
 })
